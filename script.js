cf6fb54e0782f7149457e5ab51cd21ae8746eaa6
Jasur8687 <madaminbekov908@gmail.com>
2025-11-24 21:45:05 +0500

    script.js

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ function selectLanguage(lang) {
 
   const linksByLang = {
     en: {
-      play: 'https://1wsbeh.com/?open=register&p=1ogz',
+      play: 'https://1whixd.com/?open=register&p=1ogz',
       guide: 'https://t.me/skript1win_bot'
     },
     ru: {
-      play: 'https://1wsbeh.com/?open=register&p=1ogz',
+      play: 'https://1whixd.com/?open=register&p=1ogz',
       guide: 'https://t.me/skript1win_bot'
     },
     tj: {
-      play: 'https://1wsbeh.com/?open=register&p=1ogz',
+      play: 'https://1whixd.com/?open=register&p=1ogz',
       guide: 'https://t.me/skript1win_bot'
     },
     uz: {
-      play: 'https://1wsbeh.com/?open=register&p=1ogz',
+      play: 'https://1whixd.com/?open=register&p=1ogz',
       guide: 'https://t.me/skript1win_bot'
     },
   };
